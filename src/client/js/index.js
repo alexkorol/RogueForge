8ddbbf5e709199913ef0b2b85d2fa226f87697cc
Alexei Korol <korolalexei@gmail.com>
2023-04-14 19:32:13 +0400

    Cleaning up .cache/ folder

--- src/client/js/index.js
+++ src/client/js/index.js
@@ -1,32 +1,85 @@
 import Phaser from 'phaser';
 import BootScene from './game/scenes/BootScene';
 import MainScene from './game/scenes/MainScene';
-import { getWeb3 } from './lib/metamask';
+import logoSrc from '../assets/logo.png';
+import detectEthereumProvider from '@metamask/detect-provider';
 
 (async () => {
+  const logo = document.getElementById('logo');
+  logo.src = logoSrc;
 
-  const connectButton = document.getElementById('my-connect-button');
-  connectButton.onclick = async () => {
-    console.log('Button clicked');
-    const web3 = await getWeb3();
-    if (!web3) return;
+  const provider = await detectEthereumProvider();
 
-  const config = {
-    type: Phaser.AUTO,
-    width: 800,
-    height: 600,
-    parent: 'game-container',
-    scene: [BootScene, MainScene],
-    physics: {
-      default: 'arcade',
-      arcade: {
-        gravity: { y: 0 },
-        debug: false,
+  if (provider) {
+    startApp(provider);
+  } else {
+    console.log('Please install MetaMask!');
+  }
+
+  function startApp(provider) {
+    if (provider !== window.ethereum) {
+      console.error('Do you have multiple wallets installed?');
+    }
+  }
+
+  const chainId = await window.ethereum.request({ method: 'eth_chainId' });
+
+  window.ethereum.on('chainChanged', handleChainChanged);
+
+  function handleChainChanged(chainId) {
+    window.location.reload();
+  }
+
+  let currentAccount = null;
+  window.ethereum.request({ method: 'eth_accounts' })
+    .then(handleAccountsChanged)
+    .catch((err) => {
+      console.error(err);
+    });
+
+  window.ethereum.on('accountsChanged', handleAccountsChanged);
+
+  function handleAccountsChanged(accounts) {
+    if (accounts.length === 0) {
+      console.log('Please connect to MetaMask.');
+    } else if (accounts[0] !== currentAccount) {
+      currentAccount = accounts[0];
+    }
+  }
+
+  const ethereumButton = document.getElementById('my-connect-button');
+  ethereumButton.addEventListener('click', () => {
+    getAccount();
+  });
+
+  async function getAccount() {
+    const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
+      .catch((err) => {
+        if (err.code === 4001) {
+          console.log('Please connect to MetaMask.');
+        } else {
+          console.error(err);
+        }
+      });
+    const account = accounts ? accounts[0] : null;
+
+    const config = {
+      type: Phaser.AUTO,
+      width: 800,
+      height: 600,
+      parent: 'game-container',
+      scene: [BootScene, MainScene],
+      physics: {
+        default: 'arcade',
+        arcade: {
+          gravity: { y: 0 },
+          debug: false,
+        },
       },
-    },
-  };
+    };
 
     const game = new Phaser.Game(config);
-    connectButton.style.display = 'none';
-  };
+    ethereumButton.style.display = 'none';
+  }
+
 })();
